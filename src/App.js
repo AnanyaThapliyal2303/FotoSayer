d606ca2a672a049eb7730705a1cab556982d5c22
Ananya <59766849+AnanyaThapliyal2303@users.noreply.github.com>
2021-03-30 02:37:45 +0530

    added Authentication form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import { db } from './firebase';
+import { auth, db } from './firebase';
+import {makeStyles} from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
 import Post from './Post';
+import { Button, Input } from '@material-ui/core';
+
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+  
 
 function App() {
+    const classes= useStyles();
+    const [modalStyle]=useState(getModalStyle);
+
     const [posts, setPosts]=useState([]);
+    const [open, setOpen]=useState(false); 
+    const [username, setUsername]=useState(''); 
+    const [password, setPassword]=useState(''); 
+    const [email, setEmail]=useState(''); 
+    //creating a modal so that when user clicks button, a pop up is shown
 
-// useEffect Runs a piece of code based on a specific condition
-
+    
+    // useEffect Runs a piece of code based on a specific condition
 useEffect(()=>{
     //this is where code runs
 db.collection('posts').onSnapshot(snapshot =>{
@@ -15,12 +49,59 @@ db.collection('posts').onSnapshot(snapshot =>{
     setPosts(snapshot.docs.map(doc => ({
         id: doc.id, 
         post: doc.data()
-    }))); //map allows to loop through a lot of documents like for loop
+    }))); 
+    //map allows to loop through a lot of documents like for loop
 })
-},[]); //it would run once when the app component loads but also everytime the variable '' changes
+},[]); 
+    //it would run once when the app component loads but also everytime the variable '' changes
+
+    const signUp=(event)=>{
+        event.preventDefault();
+
+        auth.createUserWithEmailAndPassword(email, password) //being passed from state variables
+        .then()
+        .catch((error)=>alert(error.message))
+    }
 
     return ( 
-    <div className = "App" >
+    <div className = "app" >
+        <Modal
+        open={open}
+        onClose={()=> setOpen(false)}
+      >
+    <div style={modalStyle} className={classes.paper}>
+        <form className="app__signup">
+        <center>
+          <img
+            className="app__headerImage"
+            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+            alt=""
+            />
+      </center>
+            <Input
+            placeholder="usernames"
+            type="username"
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)}
+            />
+            <Input
+            placeholder="email"
+            type="text"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+            />
+            <Input
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>Sign Up</Button>
+     
+        </form>
+      
+    </div>
+      </Modal>
         <div className = "app__header" >
             <img 
               className = "app__headerImage"
@@ -28,6 +109,8 @@ db.collection('posts').onSnapshot(snapshot =>{
               alt = "insta" 
             />
         </div>
+
+        <Button onClick={()=> setOpen(true)}>Sign up</Button>
         
         <h1> Let 's build an Instagram Clone with React</h1>
 
