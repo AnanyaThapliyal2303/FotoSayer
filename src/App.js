2930924ebb0bf72672e830200716241c6fa34d17
Ananya <59766849+AnanyaThapliyal2303@users.noreply.github.com>
2021-03-29 17:26:29 +0530

    Started project- Created header

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return ( <div className = "App" >
+        <div className = "app__header" >
+        <img className = "app__headerImage"
+        src = "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        alt = "" />
+        </div>
+         <h1> Let 's build an Instagram Clone with React</h1>
+
+<h1>hello</h1>
+
+        </div>
+    );
+}
+
+export default App;
