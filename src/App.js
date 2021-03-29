fa21ac0d957d45fa9035142502cc33fa416feb71
Ananya <59766849+AnanyaThapliyal2303@users.noreply.github.com>
2021-03-29 19:58:16 +0530

    added props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
+import Post from './Post';
 
 function App() {
-    return ( <div className = "App" >
+    return ( 
+    <div className = "App" >
         <div className = "app__header" >
-        <img className = "app__headerImage"
-        src = "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-        alt = "" />
+            <img 
+              className = "app__headerImage"
+              src = "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+              alt = "insta" 
+            />
         </div>
-         <h1> Let 's build an Instagram Clone with React</h1>
+        
+        <h1> Let 's build an Instagram Clone with React</h1>
 
-<h1>hello</h1>
+        <Post username='ananya.thapliyal' caption=" Hello Cutiee..." imageUrl='https://petavistaaquaculture.in/team/Dwitrisha.jpeg'/>
+        <Post username='dwitrisha' caption=" Hello" imageUrl='https://www.ikea.com/in/en/images/products/smycka-artificial-flower-rose-red__0903311_pe596728_s5.jpg'/>
+        <Post username='swim.trish' caption=" World" imageUrl='https://www.thespruce.com/thmb/XuFXZRtRZTcA4qkm4WMc_dTkO8s=/2832x2832/smart/filters:no_upscale()/gazania-flowers-1315701_10-d5712d24749940ee90fab455ed9d0562.JPG'/>
 
         </div>
     );
--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './Post.css';
+import Avatar from "@material-ui/core/Avatar";
 
-function Post() {
-    return ( <div>
+function Post({username, caption, imageUrl}) {
+    return ( 
+        <div className="post">
+            <div className="post__header"> 
+               <Avatar
+                   className="post__avatar"
+                   alt='Ananya'//{username}
+                   src="/static/images/avatar/1.jpg"
+                />  
+            <h3>{username}</h3>
+            </div>  
 
+            <img className="post__image" src={imageUrl} />
+          {/*image*/}
+
+  <h4 className="post__text"><strong>{username}</strong>{caption}</h4>     
+     {/*username+caption*/}
         </div>
     )
 }
